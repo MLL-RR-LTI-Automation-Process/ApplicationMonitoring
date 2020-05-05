c34edabbacd3608c8c54aede8d61e52a586d5e3a
BPodder <BPodder@DESKTOP-STNNOJM>
2020-05-05 16:50:01 +0530

    -Create Onion architecture refering https://blog.thedigitalgroup.com/understanding-onion-architecture

---
+++ ServiceMonitoring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMonitoring
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
